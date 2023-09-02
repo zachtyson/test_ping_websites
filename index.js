b5aa0c2b96f92bcea3755b3d659fb3b137e21a85
Zachary <tysonzachary@ufl.edu>
2023-09-02 00:43:47 -0400

    try catch

--- index.js
+++ index.js
@@ -6,21 +6,22 @@ const TIMEOUT_SECONDS = 30;
 let numPings = 0;
 
 const pingGoogle = async () => {
-    let res = await ping.promise.probe(GOOGLE_HOST, {
-        timeout: TIMEOUT_SECONDS
-    });
-    numPings++;
-    if (!res.alive) {
-        console.log(`Failed to ping ${GOOGLE_HOST} within ${TIMEOUT_SECONDS} seconds.`);
-        console.log(`Number of pings: ${numPings}`);
-        process.exit(1);
-    } else {
-        console.log(`Successfully pinged ${GOOGLE_HOST}!`);
-        console.log(`Number of pings: ${numPings}`);
+    try {
+        let res = await ping.promise.probe(GOOGLE_HOST, {
+            timeout: TIMEOUT_SECONDS
+        });
+        numPings++;
+
+        if (!res.alive) {
+            console.log(`Failed to ping ${GOOGLE_HOST} within ${TIMEOUT_SECONDS} seconds.`);
+        } else {
+            console.log(`Successfully pinged ${GOOGLE_HOST}! Round-trip time: ${res.time}ms`);
+        }
+    } catch (error) {
+        console.error(`Error while pinging ${GOOGLE_HOST}: ${error.message}`);
     }
+    console.log(`Number of pings: ${numPings}`);
 };
 
 setInterval(pingGoogle, INTERVAL_MS);
-
-
 pingGoogle();
