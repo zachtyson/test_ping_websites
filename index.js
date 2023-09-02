d71d6e37da9ed3727cc3c90eddee92a3bf83eb3d
Zachary <tysonzachary@ufl.edu>
2023-09-02 00:41:50 -0400

    pings

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const ping = require('ping');
 const GOOGLE_HOST = 'google.com';
 const INTERVAL_MS = 5 * 60 * 1000;
 const TIMEOUT_SECONDS = 30;
+let numPings = 0;
 
 const pingGoogle = async () => {
     let res = await ping.promise.probe(GOOGLE_HOST, {
         timeout: TIMEOUT_SECONDS
     });
-
+    numPings++;
     if (!res.alive) {
         console.log(`Failed to ping ${GOOGLE_HOST} within ${TIMEOUT_SECONDS} seconds.`);
+        console.log(`Number of pings: ${numPings}`);
         process.exit(1);
     } else {
         console.log(`Successfully pinged ${GOOGLE_HOST}!`);
+        console.log(`Number of pings: ${numPings}`);
     }
 };
 
